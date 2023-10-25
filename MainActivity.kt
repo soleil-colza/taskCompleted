fab927c1443579c900b7a80215fa14640042fe32
Hina Nakahira / 中平陽菜 <soleil.colza@icloud.com>
2023-10-25 23:47:16 +0900

    Initial commit

---
+++ MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.taskcompleted
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskcompleted.ui.theme.TaskCompletedTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskCompletedTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Body()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Body(){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+            ){
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = "Checked icon"
+        )
+        Text(
+            "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(24.dp, 8.dp),
+        )
+        Text(
+            "Nice work!",
+            fontSize = 16.sp,
+        )
+    }
+}
